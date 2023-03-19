cdfdef208d0330de47611f7edd72f279ea81f4c0
Sachin <sachinbajaj0001@gmail.com>
2023-03-19 23:30:36 +0530

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = "Sachin"
+  const isLoggedIn=false;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
+        {isLoggedIn && <p>Hello {name}</p>}
+        {!isLoggedIn && <p>Hello World</p>}
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React ..
         </a>
       </header>
     </div>
